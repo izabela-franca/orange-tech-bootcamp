c057d1efeedae26f9a0211c8f28d7ac885899bf5
Izabela França <ifranca.sc@gmail.com>
2023-01-19 21:37:15 -0300

    "adicionando tipos de variaveis"

--- 02_Aprendendo a Linguagem Java/tipos-variaveis/src/TiposVariaveis.java
+++ 02_Aprendendo a Linguagem Java/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Variáveis
+        int idade = 25;
+        double altura = 1.70;
+        boolean estudante = false;
+        long cpf = 123456789L;
+        String endereco = "Avenida 9 de julho";
+
+        //Constantes
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
