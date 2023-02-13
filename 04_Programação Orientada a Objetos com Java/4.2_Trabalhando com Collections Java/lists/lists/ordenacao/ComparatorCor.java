a5ca9ede6820041a27856fd0e3ae50dd2eac1828
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2023-02-13 09:21:25 -0300

    Add files via upload

---
+++ 04_Programação Orientada a Objetos com Java/4.2_Trabalhando com Collections Java/lists/lists/ordenacao/ComparatorCor.java
@@ -0,0 +1,11 @@
+package lists.ordenacao;
+
+import java.util.Comparator;
+
+class ComparatorCor implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        return g1.getCor().compareToIgnoreCase(g2.getCor());
+    }
+}
