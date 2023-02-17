32df8f4c656b37a98a05a310f7cacaa62524a962
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2023-02-17 12:52:50 -0300

    Add files via upload

---
+++ 04_Programação Orientada a Objetos com Java/4.3_Debugging Java/CalculadoraDeMedias.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/*
+----- Debug -----
+- Adicionar um breakpoint;
+- Entrar no modo Debug;
+- Step over (próxima linha) x Step into (entra no método);
+-
+
+
+ */
+
+
+public class CalculadoraDeMedias {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        double media = calculaMediaDaTurma(alunos, scan);
+
+        System.out.printf("Média da turma %.1f", media);
+
+    }
+
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
+
+        double soma = 0;
+        for(String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+
+        return soma / alunos.length;
+    }
+}
