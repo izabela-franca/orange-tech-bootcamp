da558c994f6b350bf544adc3570b54c2d5361b2f
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2023-02-20 17:59:33 -0300

    Add files via upload

---
+++ 04_Programação Orientada a Objetos com Java/4.4_Tratamento de Exceções/ImpossivelAberturaDeArquivoException.java
@@ -0,0 +1,12 @@
+class ImpossivelAberturaDeArquivoException extends Exception {
+
+    private String nomeDoArquivo;
+    private String diretorio;
+
+    public ImpossivelAberturaDeArquivoException(String nomeDoArquivo, String diretorio) {
+        super("O arquivo " + nomeDoArquivo + " não foi encontrado no diretório " + diretorio);
+        this.nomeDoArquivo = nomeDoArquivo;
+        this.diretorio = diretorio;
+    }
+
+}
